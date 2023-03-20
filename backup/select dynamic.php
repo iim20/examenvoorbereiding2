76d0a4a12f7b1aea8b37325c150e8c56fc076706
Ilyas Mohamed <72143533+iim20@users.noreply.github.com>
2023-03-20 23:47:59 +0100

    Update select dynamic.php

--- backup/select dynamic.php
+++ backup/select dynamic.php
@@ -12,3 +12,165 @@ method="POST" action="{{ route('employee.question.enquete') }}"
                                             </select>
                                         </td>
                                     </tr>
+                                    
+                                    
+                                    
+                                    //probeer deze code voor add vraag
+                                    To accomplish this in a Laravel project, you will need to do the following:
+
+Create a button that triggers the pop-up modal. You can use a Bootstrap modal or any other modal plugin for this.
+In the modal, fetch the list of questions from the enquête using AJAX. You will need to create a controller method that retrieves the questions and returns them in a JSON format. Here's an example of how you can do this:
+php
+Copy code
+public function getQuestions()
+{
+    $questions = Enquete::find($enquete_id)->questions;
+    return response()->json($questions);
+}
+In the modal, display the list of questions with checkboxes next to each one.
+When the user selects one or more questions and clicks the "Add" button, use AJAX to send the selected questions back to the server. You will need to create another controller method to handle this request. Here's an example:
+php
+Copy code
+public function addQuestions(Request $request)
+{
+    $enquete = Enquete::find($request->input('enquete_id'));
+    $selected_questions = $request->input('selected_questions');
+
+    foreach ($selected_questions as $question_id) {
+        $question = Question::find($question_id);
+        $enquete->questions()->attach($question);
+    }
+
+    return response()->json(['success' => true]);
+}
+In the addQuestions method, retrieve the enquête and the selected questions, and use the attach() method to add the questions to the enquête. Finally, return a JSON response indicating whether the operation was successful.
+After the AJAX request is complete, update the enquête page to reflect the new list of questions.
+I hope this helps! Let me know if you have any further questions.
+
+
+
+Ilyas Mohamed
+How to make step 2 of your above answer?
+
+Sure, here's an example of how you can fetch the list of questions from the enquête using AJAX:
+
+In your view file, add a button that triggers the modal and an empty <ul> element to display the questions:
+bash
+Copy code
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addQuestionsModal">Add Questions</button>
+
+<ul id="questionsList"></ul>
+Add a script to handle the AJAX request when the modal is opened:
+php
+Copy code
+<script>
+$(function() {
+    $('#addQuestionsModal').on('show.bs.modal', function(e) {
+        var enquete_id = $('#enquete_id').val(); // replace with the ID of the enquête
+
+        $.ajax({
+            type: 'GET',
+            url: '/getQuestions',
+            data: {
+                enquete_id: enquete_id
+            },
+            success: function(data) {
+                var questions = data.questions;
+
+                $.each(questions, function(index, question) {
+                    var li = $('<li/>', {
+                        'class': 'list-group-item'
+                    }).appendTo($('#questionsList'));
+
+                    $('<label/>', {
+                        'class': 'checkbox-inline'
+                    }).appendTo(li).append(
+                        $('<input/>', {
+                            type: 'checkbox',
+                            name: 'selected_questions[]',
+                            value: question.id
+                        })
+                    ).append(' ' + question.question_text);
+                });
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+            }
+        });
+    });
+});
+</script>
+In your controller, add a method to retrieve the questions and return them in a JSON format:
+php
+Copy code
+public function getQuestions(Request $request)
+{
+    $enquete_id = $request->input('enquete_id');
+    $questions = Enquete::find($enquete_id)->questions;
+
+    return response()->json(['questions' => $questions]);
+}
+This script will make an AJAX request to the /getQuestions route when the modal is opened, passing the enquête ID as a parameter. The controller method will retrieve the questions for that enquête and return them in a JSON format, which will then be used to populate the list of checkboxes in the modal. When the user selects one or more questions and clicks the "Add" button, you can use AJAX to send the selected questions back to the server for processing, as explained in step 4 of my previous answer.
+
+
+
+Ilyas Mohamed
+And how to code step 3 of above answer?
+
+Certainly, here's an example of how you can handle the "Add" button click event and send the selected questions back to the server using AJAX:
+
+Add a "Save" button to the modal:
+python
+Copy code
+<div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+    <button type="button" class="btn btn-primary" id="saveQuestionsBtn">Save</button>
+</div>
+Add a script to handle the "Add" button click event and send the selected questions back to the server using AJAX:
+php
+Copy code
+<script>
+$(function() {
+    $('#saveQuestionsBtn').click(function() {
+        var enquete_id = $('#enquete_id').val(); // replace with the ID of the enquête
+        var selected_questions = $('input[name="selected_questions[]"]:checked').map(function() {
+            return this.value;
+        }).get();
+
+        $.ajax({
+            type: 'POST',
+            url: '/addQuestions',
+            data: {
+                enquete_id: enquete_id,
+                selected_questions: selected_questions
+            },
+            success: function(data) {
+                $('#addQuestionsModal').modal('hide');
+                location.reload(); // refresh the page to reflect the changes
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+            }
+        });
+    });
+});
+</script>
+In your controller, add a method to handle the AJAX request and add the selected questions to the enquête:
+php
+Copy code
+public function addQuestions(Request $request)
+{
+    $enquete_id = $request->input('enquete_id');
+    $selected_questions = $request->input('selected_questions');
+
+    $enquete = Enquete::find($enquete_id);
+
+    foreach ($selected_questions as $question_id) {
+        $question = Question::find($question_id);
+        $enquete->questions()->attach($question);
+    }
+
+    return response()->json(['success' => true]);
+}
+This script will handle the "Add" button click event and send the selected questions back to the server using AJAX, passing the enquête ID and the selected question IDs as parameters. The controller method will retrieve the enquête and the selected questions, and use the attach() method to add the questions to the enquête. Finally, a JSON response indicating whether the operation was successful will be returned. If the operation was successful, the modal will be hidden and the page will be refreshed to reflect the changes.
+                                    
