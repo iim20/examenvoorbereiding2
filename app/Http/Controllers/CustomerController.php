5a2e81169b6943fe0c117695b295e684212198fa
Black Hole Recordings <maxa_med_2@hotmail.com>
2023-03-21 22:26:27 +0100

    Fixed klant dashboard showing many2many

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\QtsEnquete;
 
 
 
@@ -15,7 +16,8 @@ class CustomerController extends Controller
         $role=Auth::user()->is_employee;
         $users = User::all();
         if($role==0){
-            return view('customer.index');
+            $qtsEnquetes= QtsEnquete::all();
+            return view('customer.index', compact('qtsEnquetes'));
         }
         else{
             return back()->with('unauthenticated', 'Je hebt geen toegang');
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -9,5 +9,31 @@
             <span class="font-medium">{{ session('unauthenticated') }}</span>
         </div>
     @endif
-<h1 class="uppercase">customers dashboard</h1>
+    <h1 class="uppercase mb-10">customers dashboard</h1>
+
+
+
+@php
+    $enquetes = [];
+@endphp
+
+@foreach($qtsEnquetes as $enquete)
+    @if (!in_array($enquete->enquete->id, $enquetes))
+        @php
+            array_push($enquetes, $enquete->enquete->id);
+        @endphp
+        <div class="flex items-center space-x-2">
+            <p class="text-3xl">{{$enquete->enquete->id}}:</p>
+            <p class="text-3xl">{{$enquete->enquete->title}}</p>
+        </div>
+        @foreach($qtsEnquetes as $qtsEnquete)
+            @if ($qtsEnquete->enquete->id === $enquete->enquete->id)
+                    <p>{{$qtsEnquete->question->question}}</p>
+            @endif
+        @endforeach
+    @endif
+@endforeach
+
+
+
 @endsection
